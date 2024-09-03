21b4c03553e098398b98b10caa976e9db89d161f
gumarshams <gumarshams>
2024-09-03 16:13:22 +0300

    first coomit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let surname = prompt('ваша фамилия', 'фамилия');
+let name1 = prompt('ваше имя', 'имя');
+let secondName = prompt('ваша отчество', 'отчество');
+alert(`ваша фамилия ${surname}`);
+alert(`ввше имя ${name1}`);
+alert(`ваше отчество ${secondName}`);
+let isTrue = confirm("правильно ли ваше ФИО");
+alert(isTrue);
